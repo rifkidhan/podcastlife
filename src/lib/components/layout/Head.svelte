e5d0bf31062b3ebc62c6d3b89620a85adcd5383a
Rifki Ramadhan <rifkiramadhan.9428@hotmail.com>
2024-09-29 02:46:48 +0700

    update head element

--- src/lib/components/layout/Head.svelte
+++ src/lib/components/layout/Head.svelte
@@ -44,6 +44,18 @@
 
 <svelte:head>
 	<title>{title}</title>
+	<meta name="title" content={title} />
 	<meta name="description" content={description} />
 	<link rel="canonical" href={$page.url.href} />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content={$page.url.href} />
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+	<meta property="og:image" content={image} />
+
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content={$page.url.href} />
+	<meta property="twitter:title" content={title} />
+	<meta property="twitter:description" content={description} />
+	<meta property="twitter:image" content={image} />
 </svelte:head>
