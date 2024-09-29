ce645acb6ecfd540e9b750e07b6dd5bc6aeb4040
Rifki Ramadhan <rifkiramadhan.9428@hotmail.com>
2024-09-29 17:25:07 +0700

    use date now

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import * as child_process from 'node:child_process';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,7 +8,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		version: {
-			name: child_process.execSync('git rev-parse HEAD').toString().trim(),
+			name: Date.now().toString(),
 			pollInterval: 15_000
 		}
 	}
